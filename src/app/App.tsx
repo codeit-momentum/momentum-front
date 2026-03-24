a4dda119813f499c3737e8c593d1e21e0b1493e2
P1su <pissssssu@naver.com>
2026-03-24 20:32:49 +0900

    chore: 라우팅 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from 'react-router';
+import router from './routes/Router';
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/app/routes/Router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router';
+import { LoginPage, MainPage } from '@/pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: '/main',
+        element: <MainPage />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
