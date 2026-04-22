ead22f9c8ebd091b99aec372031f6b3a64dff6d8
P1su <pissssssu@naver.com>
2026-04-22 18:08:46 +0900

    style: 사용하지 않는 파일 삭제 및 메인 페이지 문구 변경

--- src/pages/Main/ui/MainPage/MainPage.tsx
+++ src/pages/Main/ui/MainPage/MainPage.tsx
@@ -4,7 +4,7 @@ export function MainPage() {
   return (
     <S.MainPageLayout>
       <div>
-        업그레이드 된 플레이 게임세계에 입장하기 전 떨림과 설레임 1234567890
+        메인 페이지
       </div>
     </S.MainPageLayout>
   );
